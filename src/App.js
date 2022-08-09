47fe5c2847f6d87ffaed026b4480d64914b086f9
SemahaDundar <semahadundaruyar94@gmail.com>
2022-08-09 18:58:24 +0300

    event baslangıc ornekleri

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from "react";
 
-import './App.css';
+
+
 
 function App() {
+
+
+
+  const[bilgi , bilgiGuncelle] = useState("");
+
+
+
+  const fonksiyon1=()=>{
+    console.log("Buton tıklandı.");
+    }
+const fonksiyon2 =(e)=>{
+  bilgiGuncelle(e.target.value);
+
+}    
   return (
+  <>
    <p>Merhabalarr!</p>
+   <input onChange={fonksiyon2} type="text"/>
+   <button onClick={fonksiyon1}>Uyari</button>
+  <p> {bilgi.length > 10 ? "10 karakteri gecemez" : "" }</p>
+  </>
   );
 }
 
