910510c494c90f99ad365ff5ec43b96b2eeb2370
SemahaDundar <semahadundaruyar94@gmail.com>
2022-08-11 14:44:27 +0300

    form select ve textarea ornekleri yapıldı.

--- src/Iletisim.jsx
+++ src/Iletisim.jsx
@@ -3,101 +3,121 @@ import { useState } from 'react';
 function Iletisim() {
 
 
-    const [formAlanlari, formAlanlariGuncelle] = useState("");
+    const [formAlanlari, formAlanlariGuncelle] = useState({});
 
-    const topluGuncelleme =(event) =>{
+    const topluGuncelleme = (event) => {
         const name = event.target.name;
         const value = event.target.value;
-        formAlanlariGuncelle(values =>({...values, [name]: value}))
+        formAlanlariGuncelle(values => ({ ...values, [name]: value }))
 
 
     }
- 
- 
 
 
     const formGonder = (olay) => {
         olay.preventDefault();//formun tarayıcı tarafında gönderme işlemi durdurma fonkiyonu
 
 
-        if (formAlanlari.isim.length > 10) {
-            alert("Dikkat! Uzunluk 10'dan büyük olamaz")
+
+        if (!formAlanlari.isim || formAlanlari.isim.length < 2){
+            alert ("İsim uzunluğu 2'den küçük olamaz");
+            return;
+        }
+
+        if (formAlanlari.isim?.length > 10) {
+            alert("Dikkat! İsim uzunlugu 10'dan büyük olamaz");
             return;
 
         }
 
-        if (formAlanlari.soyad.length > 10) {
-            alert("Dikkat! Uzunluk  2'den küçük olamaz")
+        if (formAlanlari.soyad?.length > 10) {
+            alert("Dikkat! Uzunluk  2'den küçük olamaz");
             return;
         }
-    }
+    
 
-    if (formAlanlari.telefon.length > 10) {
-        alert("Dikkat! 10 haneli numaranızı giriniz")
+    if (formAlanlari.telefon?.length > 10) {
+        alert("Dikkat! 10 haneli numaranızı giriniz");
         return;
     }
 
 
-    if (formAlanlari.site.length > 100) {
-        alert("Dikkat! 100'den büyük olamaz ")
+    if (formAlanlari.site?.length > 100) {
+        alert("Dikkat! 100'den büyük olamaz ");
         return;
     }
+
+
+    alert("Mesaj Gönderildi");
 }
-            alert("Mesaj Gönderildi");
-
-    return (
-        <>
-            <form onSubmit={formGonder}>
-                <div><p>İsim:</p>
-                    <input name='ad' type="text " onChange={topluGuncelle} />
-                    {formAlanlari?.isim?.length}
-
-                    <button type="submit">Gönder</button>
 
 
-
-                </div>
-                <div><p>Soyad:</p>
-                    <input name='soyad' type="text " onChange={ topluGuncelle} />
-                    {formAlanlari?.soyad?.length}
-
-                    <button type="submit">Gönder</button>
+return (
+    <>
+        <form onSubmit={formGonder}>
+            <div>
+                <p>İsim:</p>
+                <input name='ad' type="text " onChange={topluGuncelleme} />
+                {formAlanlari?.isim?.length}
+    </div>
+            <div><p>Soyad:</p>
+                <input name='soyad' type="text " onChange={topluGuncelleme} />
+                {formAlanlari?.soyad?.length}
 
 
 
-                </div>
+            </div>
 
 
-                <div><p>Telefon:</p>
-                    <input name='telefon' type="text " onChange={topluGuncelle} />
-                    {formAlanlari?.telefon?.length}
-
-                    <button type="submit">Gönder</button>
-
-
-
-                </div>
-
-        
-                <div><p>Site Adres:</p>
-                    <input name='site' type="text " onChange={topluGuncelle} />
-                    {formAlanlari?.site?.length}
-
-                    <button type="submit">Gönder</button>
+            <div><p>Telefon:</p>
+                <input name='telefon' type="text " onChange={topluGuncelleme} />
+                {formAlanlari?.telefon?.length}
 
 
 
-                </div>
+
+            </div>
+
+
+            <div><p>Site Adres:</p>
+                <input name='site' type="text " onChange={topluGuncelleme} />
+                {formAlanlari?.site?.length}
 
 
 
-            </form>
+            </div>
+
+            <div><p>Bizi Nereden Duydunuz?</p>
+                <select name='neredenduyuldu' onChange={topluGuncelleme}>
+                    <option value="Google">Google</option>
+                    <option value="Arkadaş">Arkadaş</option>
+                    <option value="İnstagram">İnstagram</option>
+                </select>
 
 
-        </>
 
-    )
+            </div>
+
+            <div><p>Mesaj:</p>
+                <textarea name='mesaj' type="text " onChange={topluGuncelleme} />
+                {formAlanlari?.mesaj?.length}
+
+                
 
 
 
+            </div>
+
+    
+            <button type="submit">Gönder</button>
+
+        </form>
+
+
+    </>
+
+)
+
+
+}
 export default Iletisim;
